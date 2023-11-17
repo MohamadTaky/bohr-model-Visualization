937f5cd4fa74aaaa542ba14d87a5cd09ec2d7f0f
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-11-17 23:19:07 +0200

    added generateOrbit function and automatic nucleus particles distribution through generateNucleus function

--- src/components/generateAtom.ts
+++ src/components/generateAtom.ts
@@ -5,14 +5,14 @@ import generateParticle from "./generateParticle";
 
 const up = new THREE.Vector3(0, 1, 0);
 const shellDistance = [3, 6, 9];
-const shellResloution = 100;
 
 export default function generateAtom(atom: keyof typeof atoms) {
-	const lines = [];
+	const orbits = [];
 	const shells = [];
 	const electrons = [];
 
 	const atomConfig = atoms[atom];
+
 	const electronConfiguration = atomConfig.electron_configuration.split(" ");
 	for (let i = 0; i < electronConfiguration.length; i++) {
 		const segment = getSegmentConfig(electronConfiguration[i]);
@@ -23,6 +23,7 @@ export default function generateAtom(atom: keyof typeof atoms) {
 	}
 
 	for (let i = 0; i < electrons.length; i++) {
+		//#region electrons generation
 		for (let j = 0; j < electrons[i]; j++) {
 			const electron = generateParticle("electron");
 			electron.userData.dir
@@ -33,22 +34,29 @@ export default function generateAtom(atom: keyof typeof atoms) {
 			if (shells[i]) shells[i].push(electron);
 			else shells[i] = [electron];
 		}
+		//#endregion
 
-		const points = [];
-		for (let j = 0; j < shellResloution; j++)
-			points.push(
-				new THREE.Vector3(1, 0, 0)
-					.applyAxisAngle(up, degToRad((360 / shellResloution) * j))
-					.multiplyScalar(shellDistance[i])
-			);
-		const geomentry = new THREE.BufferGeometry().setFromPoints(points);
-		const material = new THREE.LineBasicMaterial();
-		const mesh = new THREE.LineLoop(geomentry, material);
-		lines.push(mesh);
+		const orbit = generateOrbit(shellDistance[i]);
+		orbits.push(orbit);
 	}
 
-	return { lines, shells };
+	return { orbits, shells };
 }
+
+function generateOrbit(distance: number, resloution = 100) {
+	const points = [];
+	for (let j = 0; j < resloution; j++)
+		points.push(
+			new THREE.Vector3(1, 0, 0)
+				.applyAxisAngle(up, degToRad((360 / resloution) * j))
+				.multiplyScalar(distance)
+		);
+	const geomentry = new THREE.BufferGeometry().setFromPoints(points);
+	const material = new THREE.LineBasicMaterial();
+	return new THREE.LineLoop(geomentry, material);
+}
+
+
 
 function getSegmentConfig(segment: string) {
 	return {
@@ -56,4 +64,47 @@ function getSegmentConfig(segment: string) {
 		subShell: segment[1],
 		electrons: +segment[2],
 	};
+}
+
+export function generateNucleus(atom: keyof typeof atoms) {
+	const atomConfig = atoms[atom];
+	let t = 1;
+	let k = 100;
+
+	const zeroVector = new THREE.Vector3();
+	const particles: THREE.Mesh[] = [];
+	const l = 0.3;
+	const repulsiveConstant = 1;
+	const springConstant = 30;
+	const coolDownFactor = 0.01;
+
+	for (let i = 0; i < atomConfig.atomic_number; i++)
+		particles.push(generateParticle("proton", new THREE.Vector3().randomDirection().multiplyScalar(5)));
+	for (let i = 0; i < Math.floor(atomConfig.atomic_mass - atomConfig.atomic_number); i++)
+		particles.push(generateParticle("neutorn", new THREE.Vector3().randomDirection().multiplyScalar(5)));
+
+	while (t < k) {
+		const forces = [];
+		for (let i = 0; i < particles.length; i++) {
+			const attractiveForce = new THREE.Vector3()
+				.copy(particles[i].position)
+				.negate()
+				.multiplyScalar(springConstant * Math.log(particles[i].position.distanceTo(zeroVector) / l));
+
+			for (let j = 0; j < particles.length; j++) {
+				if (i === j) continue;
+				const repulsiveForce = new THREE.Vector3()
+					.subVectors(particles[i].position, particles[j].position)
+					.multiplyScalar(repulsiveConstant / particles[i].position.distanceToSquared(particles[j].position));
+				attractiveForce.add(repulsiveForce);
+			}
+			forces.push(attractiveForce);
+		}
+		console.log(forces);
+
+		for (let i = 0; i < particles.length; i++)
+			particles[i].position.add(forces[i].multiplyScalar(coolDownFactor));
+		t++;
+	}
+	return particles;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
-import atoms from "@/atoms";
 import camera from "@/components/MainCamera";
 import { renderer, scene } from "@/components/MainScene";
 import { deltaTime, tick } from "@/components/Time";
 import "@/style.css";
 import * as THREE from "three";
 import { degToRad } from "three/src/math/MathUtils";
-import generateAtom from "./components/generateAtom";
+import generateAtom, { generateNucleus } from "./components/generateAtom";
 
 const up = new THREE.Vector3(0, 1, 0);
-const currentAtom = generateAtom("carbon");
+const currentAtom = generateAtom("nitrogen");
+const currentNuclues = generateNucleus("nitrogen");
 
 currentAtom.shells.forEach(shell => shell.forEach(electron => scene.add(electron)));
-currentAtom.lines.forEach(line => scene.add(line));
+currentAtom.orbits.forEach(orbit => scene.add(orbit));
 
-atoms[0].nucleus.forEach(particle => scene.add(particle));
+console.log(currentNuclues[0].position);
+
+currentNuclues.forEach(particle => scene.add(particle));
 
 function draw() {
 	requestAnimationFrame(draw);
